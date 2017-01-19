01d3579416ffcf6748974cf77e5c7a85df2ba2f5
Qbabury <qbabury@gmail.com>
2017-01-19 20:48:13 +0100

    Update topo.py
    nadane maci

--- topo.py
+++ topo.py
@@ -14,14 +14,14 @@ class MyTopo(Topo):
         Topo.__init__(self)
 
         # Add hosts and switches
-        host1 = net.addHost('h1', ip='192.168.1.1')
-        host2 = net.addHost('h2', ip='192.168.1.2')
-        host3 = net.addHost('h3', ip='192.168.1.3')
-        server1 = net.addHost('s1', ip='192.168.1.11')
-        server2 = net.addHost('s2', ip='192.168.1.12')
-        server3 = net.addHost('s3', ip='192.168.1.13')
+        host1 = net.addHost('h1', ip='192.168.1.1', mac='00:00:00:00:00:01')
+        host2 = net.addHost('h2', ip='192.168.1.2', mac='00:00:00:00:00:02')
+        host3 = net.addHost('h3', ip='192.168.1.3', mac='00:00:00:00:00:03')
+        server1 = net.addHost('s1', ip='192.168.1.11', mac='00:00:00:00:00:11')
+        server2 = net.addHost('s2', ip='192.168.1.12', mac='00:00:00:00:00:12')
+        server3 = net.addHost('s3', ip='192.168.1.13', mac='00:00:00:00:00:13')
 
-        lb = net.addSwitch('lb')
+        lb = net.addSwitch('lb', mac='00:00:00:00:00:10')
 
         # Add links
         net.addLink(host1, lb)
